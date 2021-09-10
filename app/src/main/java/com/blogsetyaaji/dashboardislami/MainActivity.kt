f59535ee383c7e19855c16b415450f51b145ed1d
AjiSetya <setyaaji07@gmail.com>
2021-09-10 15:01:07 +0700

    Initial commit

---
+++ app/src/main/java/com/blogsetyaaji/dashboardislami/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.blogsetyaaji.dashboardislami
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
+
+// identifier = nama
+// menggunakan huruf kecil (gaya snake case)
+// tidak boleh pakai spasi/karakter khusus
+// diawali oleh huruf
